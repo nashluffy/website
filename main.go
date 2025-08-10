ef3c9dbd912ceb84332a3ca223de30ad9bca6aec
Nash Luffman <nashluffman@gmail.com>
2025-08-10 18:02:41 +0100

    fix bind address

--- main.go
+++ main.go
@@ -27,5 +27,5 @@ func main() {
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("static/css"))))
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("static/images"))))
 	log.Println("Starting server on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
